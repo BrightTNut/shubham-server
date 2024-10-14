cb3a537487a2a505f6981c1866a071e1b3265384
BrightTNut <118769123+BrightTNut@users.noreply.github.com>
2024-10-14 19:16:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,8 +16,4 @@ app.use(express.json());
 app.use("/api/portfolio", portfoilioRoute);
 
 
-const port = process.env.PORT || 5000
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
